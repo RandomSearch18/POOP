8ab7773203c73663a669a6e808a961744d751bdc
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-03-26 12:44:45 +0000

    Treasure: do task 2

--- Treasure island game/treasure.py
+++ Treasure island game/treasure.py
@@ -5,15 +5,28 @@ class Treasure:
     def __init__(self, value: int, level: str):
         self._value = value
         self._level = level
-    
+
     def get_value(self):
         return self._value
 
     def set_value(self, value: int):
         self._value = value
-    
+
     def get_level(self):
-        return self.level
+        return self._level
 
     def set_level(self, level: str):
         self._level = level
+
+    def __str__(self):
+        level_part = self.get_level().capitalize()
+        value_part = self.get_value()
+        return f"{level_part} treasure (value {value_part})"
+
+
+if __name__ == "__main__":
+    bronze_treasure = Treasure(100, "bronze")
+    bronze_treasure.set_value(150)
+    bronze_treasure.set_level("ultra bronze")
+
+    print(bronze_treasure)
